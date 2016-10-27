3394d28a4508ddcd0798a3f3e5f9eaa98cc46473
Cory Mickelson <cmickelson@skyslope.com>
2016-10-27 15:25:39 -0700

    Fix tests, add Trie unit tests

--- src/trie.js
+++ src/trie.js
@@ -9,93 +9,97 @@ import { Leaf } from "./node";
  * @class Trie
  */
 export class Trie {
-  /**
-   * Each new trie starts with a single root node with the value of "".
-   *
-   * @constructor
-   * @param {Array<*>} opts
-   */
-  constructor( ...opts ) {
-    if(opts.length) { }
-    else this.root = new Leaf(null, "");
-  }
+	/**
+	 * Each new trie starts with a single root node with the value of "".
+	 *
+	 * @constructor
+	 * @param {Array<*>} opts
+	 */
+	constructor( ...opts ) {
+		if ( opts.length ) {
+		}
+		else this.root = new Leaf( null, "" );
+	}
 
-  /**
-   * construct word / phrase given a final node
-   *
-   * @param { Leaf } node
-   * @param { String } build
-   * @returns Void|String
-   */
-  buildLeafFragment( node , build = "" ) {
-    if ( node.parent.key === "" ) return build;
-    else this.buildLeafFragment( node.parent , `${node.key}${build}` );
-  }
+	/**
+	 * construct word / phrase given a final node
+	 *
+	 * @param { Leaf } node
+	 * @param { String } build
+	 * @returns Void|String
+	 */
+	buildFromNode( node, build = "" ) {
+		if ( node.parent.key === "" ) return `${node.key}${build}`;
+		else return this.buildFromNode( node.parent, `${node.key}${build}` );
+	}
 
-  /**
-   * Trie.append should only be called with a string fragment that is known
-   * to not be contained in the tree.
-   *
-   * Trie.append will recursively add a new node at the parent parameter until
-   * keys has reached it's end.
-   *
-   * keys is the complete word/phrase string fragment
-   * parent is the last node found in keys
-   * @see Leaf.sortInsert
-   *
-   * @param { Leaf } parent
-   * @param { String } keys
-   */
-  append( parent , keys ) {
-    if ( keys.length === 1 ) {
-      parent.sortInsert( new Leaf( parent, keys ), [] );
-    }
-    else {
-      let n = new Leaf( parent, keys.charAt( 0 ) );
-      parent.sortInsert( n );
-      this.append( n , keys.substr( 1 ) );
-    }
-  }
+	/**
+	 * Trie.append should only be called with a string fragment that is known
+	 * to not be contained in the tree.
+	 *
+	 * Trie.append will recursively add a new node at the parent parameter until
+	 * keys has reached it's end.
+	 *
+	 * keys is the complete word/phrase string fragment
+	 * parent is the last node found in keys
+	 * @see Leaf.sortInsert
+	 *
+	 * @param { Leaf } parent
+	 * @param { String } keys
+	 */
+	append( parent, keys ) {
+		if ( keys.length === 1 ) {
+			parent.sortInsert( new Leaf( parent, keys ), [] );
+		}
+		else {
+			let n = new Leaf( parent, keys.charAt( 0 ) );
+			parent.sortInsert( n );
+			this.append( n, keys.substr( 1 ) );
+		}
+	}
 
-  /**
-   *
-   * @param { String } word
-   * @param { Leaf } parent
-   * @return Leaf - last node in query or last node in trie
-   */
-  find( word , parent = this.root ) {
-    if ( parent.final() ) return parent;
-    while(word.length > 1) {
-        this.find( word.substr( 1 ) , parent.find( word.charAt( 0 ) ).node );
-    }
-    return parent.find( word ).node;
-  }
+	/**
+	 *
+	 * @param { String } word
+	 * @param { Leaf } parent
+	 * @return Leaf - last node in query or last node in trie
+	 */
+	find( word, parent = this.root ) {
+		if ( parent.final() ) return parent;
+		if ( word.length > 1 ) {
+			let fragment = word.slice( 1 ), key = word[ 0 ]
+			return this.find( fragment, parent.find( key ).node );
+		}
+		if ( word.length === 1 ) {
+			return parent.find( word ).node;
+		}
+	}
 
-  /**
-   * Add a word/phrase to tree.
-   * caller does not know tree status.
-   *
-   * @see Leaf#buildLeafFragment
-   * @see Leaf#find
-   * @see Leaf#append
-   *
-   * @param { String } word
-   * @param { Leaf } parent
-   * @returns Void
-   */
-  addFragment(word, parent = this.root) {
-    if ( parent.final() && word.length > 0)  this.append(parent, word);
-    else if ( word.length === 1 ) {
-      let n = parent.find(word);
-      if(n.found === false) this.append(parent, word);
-    }
-    else {
-      let child = parent.find( word.charAt( 0 ) );
-      if(child.found == false) {
-        this.append(child.node, word);
-      }
-      else this.addFragment( word.substr( 1 ) , child.node);
-    }
-  }
+	/**
+	 * Add a word/phrase to tree.
+	 * caller does not know tree status.
+	 *
+	 * @see Leaf#buildFromNode
+	 * @see Leaf#find
+	 * @see Leaf#append
+	 *
+	 * @param { String } word
+	 * @param { Leaf } parent
+	 * @returns Void
+	 */
+	addFragment( word, parent = this.root ) {
+		if ( parent.final() && word.length > 0 )  this.append( parent, word );
+		else if ( word.length === 1 ) {
+			let n = parent.find( word );
+			if ( n.found === false ) this.append( parent, word );
+		}
+		else {
+			let child = parent.find( word.charAt( 0 ) );
+			if ( child.found == false ) {
+				this.append( child.node, word );
+			}
+			else this.addFragment( word.substr( 1 ), child.node );
+		}
+	}
 }
 
--- src/trie.spec.js
+++ src/trie.spec.js
@@ -1,31 +1,49 @@
-import { Trie } from "./trie.js";
-import { Test } from "tape";
+const tape = require( "tape" ),
+		subject = require( "./trie.build.js" ),
+		Trie = subject.trie.Trie
 
-/**
- * Setup testing environment.
- *
- * @returns Trie
- */
-function setup() {
-	let tree = new Trie();
-	tree.append( tree.root, "test" );
-	return tree;
-}
+tape( "Tree is created with a root node",
+		function ( t ) {
+			let tree = new Trie();
+			t.equal( "",
+					tree.root.key,
+					"Tree root key should always have a value of an empty string." );
+			t.end();
+		} );
+tape( "Tree append does not throw error.",
+		function ( t ) {
+			let tree = new Trie();
+			tree.append( tree.root, "test" )
+			t.end()
+		} );
+tape( "Tree find returns last node found in phrase.",
+		function ( t ) {
+			let tree = new Trie();
+			tree.append( tree.root, "test" );
+			let found = tree.find( "test" );
+			t.equal( "t", found.key, "found node key should be equal to last character in phrase." );
+			t.end();
+		} );
 
-export function tests() {
-	Test( "Tree is created with a root node",
-			function ( t ) {
-				let tree = setup();
-				t.equal( "",
-						tree.root.key,
-						"Tree root key should always have a value of an empty string." );
-				t.end();
-			} );
-	Test( "Tree append adds all nodes in provided word/phrase",
-			function ( t ) {
-				let tree = setup()
-				tree.append(tree.root, "test");
-				let found = tree.find( "test" );
-				t.equal( "t", found.key, "Trie.find returns correct node." );
-			} );
-}
+tape( "Tree addFragment adds phrase at last node found.",
+		function ( t ) {
+			let tree = new Trie();
+			tree.append( tree.root, "test" );
+			let lastNode = tree.find( "test" );
+			tree.addFragment( "testing" );
+			t.equal( lastNode.children.length, 1, "node added to last node found." )
+			t.equal( lastNode.children[ 0 ].children[ 0 ].children[ 0 ].key,
+					"g",
+					"word / phrase fragment insert complete." );
+			t.end()
+		} )
+
+tape( "Build word / phrase fragment from given node.",
+	function(t) {
+		let tree = new Trie();
+		tree.append(tree.root, "test");
+		let lastNode = tree.find("test"),
+				phrase = tree.buildFromNode(lastNode);
+		t.equal(phrase, "test", "Should build phrase from last node.")
+		t.end()
+	})
